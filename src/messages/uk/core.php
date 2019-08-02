1540aa735608ff3b783dc3a3461dcacb252f1921
Yurii <ripaym1970@gmail.com>
2019-08-03 01:25:40 +0300

    Add message UK language

---
+++ src/messages/uk/core.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Language 'UK'.
+ */
+return [
+    'ID' => 'ID',
+    'TITLE' => 'Заголовок',
+    'DISPLAY_TITLE' => 'Відображати заголовок',
+    'ALIAS' => 'Экшен сторінки',
+    'PUBLISHED' => 'Активно',
+    'CONTENT' => 'Зміст',
+    'TITLE_BROWSER'    => 'Meta title',
+    'META_KEYWORDS'    => 'Meta Keywords',
+    'META_DESCRIPTION' => 'Meta Description',
+    'CREATED_AT' => 'Створено',
+    'UPDATED_AT' => 'Змінено',
+    'PAGE_NOT_FOUND' => 'За вказаною адресою сторінка не знайдена.',
+    'CREATE' => 'Створити',
+    'UPDATE' => 'Змінити',
+    'MODULE_NAME' => 'Сторінки',
+    'SAVE_SUCCESS'   => 'Збереження  завершилось успішно!',
+    'DELETE_SUCCESS' => 'Видалення завершилось успішно!',
+];
